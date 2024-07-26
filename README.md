98524d8b002bf641ccaff5f5da189d2cace926ce
Breno Cabral <breno.ara.cabral@gmail.com>
2024-07-26 20:14:45 +0000

    adAciona scripipts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Projeto educacional recriando um projeto real ( https://www.tabnews.com.br ) 
+
+Projeto educacional recriando um projeto real ( https://www.tabnews.com.br )
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>A vida, se você souber usá-la, é longa! (Seneca)</h1>
+function Home() {
+  return <h1>A vida, se você souber usá-la, é longa! (Seneca)</h1>;
 }
 
 export default Home;
