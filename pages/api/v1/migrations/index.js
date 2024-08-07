7d00e1e4b659f65254a46b9ab3eb4235cf9d2c9e
Breno Cabral <breno.ara.cabral@gmail.com>
2024-08-07 11:46:24 +0000

    testing my previous solution

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -34,7 +34,7 @@ export default async function migrations(request, response) {
     return response.status(200).json(migratedMigrations);
   }
 
-  //await dbClient.end();
+  await dbClient.end();
 
   return response.status(405).end();
 }
