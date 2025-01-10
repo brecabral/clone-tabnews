f099459285531363b2ee30a8b56260fa64fa9405
Breno Cabral <breno.ara.cabral@gmail.com>
2025-01-10 15:49:19 -0300

    opa

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,41 @@
+import React, { useState, useEffect } from "react";
+
 function Home() {
-  return <h1>A vida, se você souber usá-la, é longa! (Seneca)</h1>;
+  const [timeLeft, setTimeLeft] = useState("");
+
+  useEffect(() => {
+    // Calcula o tempo alvo com 19 horas e 10 minutos a partir de agora
+    const targetTime = new Date().getTime() + (19 * 60 + 10) * 60 * 1000;
+
+    const updateTimer = () => {
+      const now = new Date().getTime();
+      const difference = targetTime - now;
+
+      if (difference > 0) {
+        const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((difference / (1000 * 60)) % 60);
+        const seconds = Math.floor((difference / 1000) % 60);
+        setTimeLeft(
+          `${hours.toString().padStart(2, "0")}:${minutes
+            .toString()
+            .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+        );
+      } else {
+        setTimeLeft("00:00:00");
+        clearInterval(timerInterval);
+      }
+    };
+
+    const timerInterval = setInterval(updateTimer, 1000);
+
+    return () => clearInterval(timerInterval);
+  }, []);
+
+  return (
+    <div>
+      <h2>Tempo restante 👰🏻‍♂️🐶: {timeLeft}</h2>
+    </div>
+  );
 }
 
 export default Home;
