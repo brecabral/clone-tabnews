e1d031fbd09704b299936cca0d8ccc75977d4d77
Breno Cabral <breno.ara.cabral@gmail.com>
2025-08-02 19:15:02 -0300

    feat: create `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,36 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors";
+import { ValidationError, NotFoundError } from "infra/errors";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+      SELECT 
+        * 
+      FROM
+        users
+      WHERE
+        LOWER(username) = LOWER($1)
+      LIMIT
+        1
+      ;`,
+      values: [username],
+    });
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "username não encontrado.",
+        action: "Verifique se username está digitado corretamente.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
 
 async function create(userIputValues) {
   await validadeUniqueEmail(userIputValues.email);
@@ -72,6 +103,7 @@ async function create(userIputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
